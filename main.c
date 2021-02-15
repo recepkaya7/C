bd576f7a0a604c66651e81a2e7e16b36855a04f8
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-02-16 00:19:12 +0300

    Matriste- Satırdaki sayıları sıralama

--- main.c
+++ main.c
@@ -3,26 +3,40 @@
 
 int main()
 {
-    int A[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int i,j;
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf("%d ",A[i][j]);
-        }printf("\n");
+    int A[3][3]= {{1,-1,3},{4,5,6},{7,8,9}};
 
-    }
-    kosegen(A,3);
+    sirala(A,3);
     return 0;
 }
-    void kosegen(int B[3][3],int N){
-    printf("\n---Matrisin Kosegeni---\n");
-    int i,j;
-    for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
-            if(i==j)
-            printf("%d ",B[i][j]);
+void sirala(int A[][3],int N)
+{
+    int i,j,temp;
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<N; j++)
+        {
+            for(int k=0; k<N-1; k++)
+            {
+                if(A[i][k]<A[i][k+1])
+                {
+                    temp=A[i][k];
+                    A[i][k]=A[i][k+1];
+                    A[i][k+1]=temp;
+                }
+            }
         }
-
+        printf("\n");
     }
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<N; j++)
+        {
+            printf("%2d ",A[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
 
 }
