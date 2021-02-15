786c49a5a722fc2f7454a04e26f0c50a63472bb6
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-02-15 21:51:15 +0300

    break -contunie -goto

--- main.c
+++ main.c
@@ -2,15 +2,35 @@
 #include <stdlib.h>
 
 int main()
-{
-   int i,sayi;
-   printf("Basamaklarina parcalamak istediginiz sayi:");
-   scanf("%d",&sayi);
-   for(i=1; ;i*=10){
-    if(sayi/i==0)
-    break;
-    int basamak=(sayi/i)%10;
-   printf("%d x %d\n",i,basamak);
-   }
+{   // break döngüyü kirar
+    printf("--break--\n");
+    int i;
+    for(i=0;i<10;i++){
+        if(i==6)
+            break;
+        printf("%d\n",i);
+    }
+
+
+
+    printf("--continue--\n");
+    for(i=0;i<10;i++){
+        if(i==6)
+            continue;
+        printf("%d\n",i);
+    }
+
+
+
+    printf("--goto--\n");
+    int a=10;
+    LOOP : do{
+        if(a==15){
+            a=a+2;
+            goto LOOP;
+        }
+        printf("a:%d\n",a);
+        a++;
+        }while(a<20);
     return 0;
 }
