f573cefe4742e4fe06fdfd856af790732c317ed7
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-02-16 00:13:38 +0300

    kare matrisin köşegenini bulan kod

--- main.c
+++ main.c
@@ -2,35 +2,27 @@
 #include <stdlib.h>
 
 int main()
-{   // break döngüyü kirar
-    printf("--break--\n");
-    int i;
-    for(i=0;i<10;i++){
-        if(i==6)
-            break;
-        printf("%d\n",i);
+{
+    int A[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",A[i][j]);
+        }printf("\n");
+
     }
-
-
-
-    printf("--continue--\n");
-    for(i=0;i<10;i++){
-        if(i==6)
-            continue;
-        printf("%d\n",i);
-    }
-
-
-
-    printf("--goto--\n");
-    int a=10;
-    LOOP : do{
-        if(a==15){
-            a=a+2;
-            goto LOOP;
-        }
-        printf("a:%d\n",a);
-        a++;
-        }while(a<20);
+    kosegen(A,3);
     return 0;
+}
+    void kosegen(int B[3][3],int N){
+    printf("\n---Matrisin Kosegeni---\n");
+    int i,j;
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            if(i==j)
+            printf("%d ",B[i][j]);
+        }
+
+    }
+
 }
