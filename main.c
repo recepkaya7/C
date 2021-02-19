318404ad26ce7364cd7a9640d52951bea87efecb
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-02-19 19:16:30 +0300

    Baklava dilimi

--- main.c
+++ main.c
@@ -3,40 +3,43 @@
 
 int main()
 {
-    int A[3][3]= {{1,-1,3},{4,5,6},{7,8,9}};
+   int k,s,yildiz,bosluk,giris;
+   printf("giris:");
+   scanf("%d",&giris);
+   yildiz=1;
+   bosluk=giris-1;
+   for(s=1;s<=giris;s++){
+        for(k=1;k<=bosluk;k++){
+            printf(" ");
+        }
+        for(k=1;k<=yildiz;k++){
+            if(k==1 || k==yildiz)
+                printf("o");
+            else
+            printf("*");
+        }printf("\n");
+        bosluk--;
+         yildiz+=2;
+   }
+    bosluk=1;
+    yildiz-=4;
+    for(s=1;s<=giris;s++){
+        for(k=1;k<=bosluk;k++){
+            printf(" ");
+        }
+        for(k=1;k<=yildiz;k++){
+            if(k==1 || k==yildiz)
+                printf("o");
+            else
+                printf("*");
+        }printf("\n");
+        bosluk++;
+        yildiz-=2;
+    }
 
-    sirala(A,3);
+
+
+
+
     return 0;
-}
-void sirala(int A[][3],int N)
-{
-    int i,j,temp;
-    for(i=0; i<N; i++)
-    {
-        for(j=0; j<N; j++)
-        {
-            for(int k=0; k<N-1; k++)
-            {
-                if(A[i][k]<A[i][k+1])
-                {
-                    temp=A[i][k];
-                    A[i][k]=A[i][k+1];
-                    A[i][k+1]=temp;
-                }
-            }
-        }
-        printf("\n");
-    }
-    for(i=0; i<N; i++)
-    {
-        for(j=0; j<N; j++)
-        {
-            printf("%2d ",A[i][j]);
-        }
-        printf("\n");
-    }
-
-
-
-
 }
