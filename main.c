7894d1e1b7833ff8eb2c7aaf9d2993517e23741d
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-05-05 15:50:05 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,43 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void ekle(int boyuta,int boyutb,char *a,char *b)
-{
-    int i;
-    a[boyuta-1]=' ';
-    for(i=boyuta; i<boyuta+boyutb; i++)
-    {
-        a[i]=b[i-boyuta];
-    }
-/*
-pazartersi 10
-sali 4
-carsamba 8
-persembe 8
-cuma 4
-cumartesi 9
-pazar 5
-*/
-
-
-}
 
 int main()
 {
-    char a[20],b[20];
-    printf("ilk string:");
-    fgets(a,20,stdin);
-    printf("ikinci string:");
-    fgets(b,20,stdin);
-    int au=strlen(a);
-    int bu=strlen(b);
-    printf("%d %d \n",au,bu);
-    ekle(au,bu,&a,&b);
-    for(int i=0; i<au+bu; i++)
-    {
-        printf("%c",a[i]);
+    int i;
+    for(i=0;i<32;i++){
+        char a=i;
+        if(!isprint(a))
+            a=' ';
+          char b=i+32;
+        if(!isprint(b))
+            b=' ';
+              char c=i+64;
+        if(!isprint(c))
+            c=' ';
+              char d=i+96;
+        if(!isprint(d))
+            d=' ';
+            printf("|%-3c %4d|%-3c %4d|%-3d %4d|%-3c %4d| ",a,i,b,i+32,c,i+64,d,i+96);
+    printf("\n");
     }
-    free(a);
-    free(b);
-    return 0;
+ return 0;
 }
